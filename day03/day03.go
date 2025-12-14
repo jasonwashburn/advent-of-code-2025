fddfe8dea01f4418074b6e3497ff009fab5ea47f
Jason Washburn <jason.washburn@gmail.com>
2025-12-14 08:11:51 -0600

    feat: add solution for day 3 part 2

--- day03/day03.go
+++ day03/day03.go
@@ -23,15 +23,32 @@ func Solve() {
 
 		batteries := getBatteriesFromBankStr(line)
 
-		highIndex, highestNum := findHighestNumber(batteries[:len(batteries)-1])
-		_, secondHighestNum := findHighestNumber(batteries[highIndex+1:])
+		highNums := make([]int, 12)
+		nextStartIndex := 0
+		for i := range highNums {
+			highIndex, highestNum := findHighestNumber(batteries[nextStartIndex : len(batteries)-(12-i-1)])
+			highNums[i] = highestNum
+			nextStartIndex += highIndex + 1
+		}
 
-		bankJoltage := (highestNum * 10) + secondHighestNum
-		fmt.Printf("Line %d: Highest: %d, Second Highest: %d, Bank Joltage: %d\n", lineIndex+1, highestNum, secondHighestNum, bankJoltage)
+		bankJoltageStr := arrayOfIntsToString(highNums)
+		bankJoltage, err := strconv.Atoi(bankJoltageStr)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Line %d: Bank Joltage: %d\n", lineIndex+1, bankJoltage)
 		totalJoltage += bankJoltage
 
 	}
 	fmt.Println("Total Joltage:", totalJoltage)
+}
+
+func arrayOfIntsToString(arr []int) string {
+	output := ""
+	for _, n := range arr {
+		output += strconv.Itoa(n)
+	}
+	return output
 }
 
 func readInputLinesFromFile(filepath string) ([]string, error) {
@@ -57,6 +74,7 @@ func getBatteriesFromBankStr(s string) []int {
 }
 
 func findHighestNumber(numbers []int) (index int, value int) {
+	fmt.Println("Finding highest in:", numbers)
 	highest := -1
 	for i, n := range numbers {
 		if n > highest {
